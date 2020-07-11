fa9f071109341bac8b147f2a026ec3cc46c1550f
lyread <noreply@github.com>
2020-07-11 18:00:28 +0200

    permissions

--- src/Lyread/Lyread.Android/MainActivity.cs
+++ src/Lyread/Lyread.Android/MainActivity.cs
@@ -6,13 +6,16 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using System.Linq;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+using System.Threading.Tasks;
 
 namespace Lyread.Droid
 {
     [Activity(Theme = "@style/SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
             SetTheme(Resource.Style.MainTheme);
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -24,24 +27,37 @@ namespace Lyread.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
 
-            if ((int)Build.VERSION.SdkInt >= 23 && CheckSelfPermission(Manifest.Permission.WriteExternalStorage) != Permission.Granted)
-            {
-                RequestPermissions(new[] { Manifest.Permission.WriteExternalStorage }, 0);
-                return;
-            }
-            LoadApplication(new App());
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            if (grantResults.First() != Permission.Granted)
+            PermissionStatus status = await CheckAndRequestReadWriteStoragePermission();
+            if (status != PermissionStatus.Granted)
             {
                 Finish();
             }
             LoadApplication(new App());
         }
+
+        public async Task<PermissionStatus> CheckAndRequestReadWriteStoragePermission()
+        {
+            var status = await Permissions.CheckStatusAsync<ReadWriteStoragePermission>();
+            if (status != PermissionStatus.Granted)
+            {
+                status = await Permissions.RequestAsync<ReadWriteStoragePermission>();
+            }
+            return status;
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public partial class ReadWriteStoragePermission : Xamarin.Essentials.Permissions.BasePlatformPermission
+    {
+        public override (string androidPermission, bool isRuntime)[] RequiredPermissions => new List<(string androidPermission, bool isRuntime)>
+        {
+            (Android.Manifest.Permission.ReadExternalStorage, true),
+            (Android.Manifest.Permission.WriteExternalStorage, true)
+        }.ToArray();
     }
 }
