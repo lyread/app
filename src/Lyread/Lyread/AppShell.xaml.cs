de466892c8bf66e7929f343dc0a0354476be929e
lyread <noreply@github.com>
2020-10-31 21:14:24 +0100

    merge

--- src/Lyread/Lyread/AppShell.xaml.cs
+++ src/Lyread/Lyread/AppShell.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Lyread.ViewModels;
+using Lyread.Views;
 using Xamarin.Forms;
 
 namespace Lyread
@@ -10,6 +11,13 @@ namespace Lyread
         public AppShell()
         {
             InitializeComponent();
+            //Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            //Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
         }
     }
 }
